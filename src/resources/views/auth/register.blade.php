f4d25024b28da62ce1724e365f7c5cc83a0b616a
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2025-07-03 15:11:04 +0900

    validation設定＆レイアウトの修正

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -18,20 +18,37 @@
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
-            <label>名前</label>
-            <input type="text" name="name" value="{{ old('name') }}" required>
-            @error('name') <p class="error">{{ $message }}</p> @enderror
+            <div class="form-group">
+                <label for="name">名前</label>
+                <input id="name" type="text" name="name" value="{{ old('name') }}">
+                @error('name')
+                <p class="error">{{ $message }}</p>
+                @enderror
+            </div>
 
-            <label>メールアドレス</label>
-            <input type="email" name="email" value="{{ old('email') }}" required>
-            @error('email') <p class="error">{{ $message }}</p> @enderror
+            <div class="form-group">
+                <label for="email">メールアドレス</label>
+                <input id="email" type="email" name="email" value="{{ old('email') }}">
+                @error('email')
+                <p class="error">{{ $message }}</p>
+                @enderror
+            </div>
 
-            <label>パスワード</label>
-            <input type="password" name="password" required>
-            @error('password') <p class="error">{{ $message }}</p> @enderror
+            <div class="form-group">
+                <label for="password">パスワード</label>
+                <input id="password" type="password" name="password">
+                @error('password')
+                <p class="error">{{ $message }}</p>
+                @enderror
+            </div>
 
-            <label>パスワード確認</label>
-            <input type="password" name="password_confirmation" required>
+            <div class="form-group">
+                <label for="password_confirmation">パスワード確認</label>
+                <input id="password_confirmation" type="password" name="password_confirmation">
+                @error('password_confirmation')
+                <p class="error">{{ $message }}</p>
+                @enderror
+            </div>
 
             <button type="submit">登録する</button>
         </form>
