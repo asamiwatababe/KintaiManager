ffae7ff591cf836f8a53d9d6d9605fe1e5d57bb4
asamiwatababe <sho3tsu4@yahoo.ne.jp>
2025-08-02 12:32:57 +0900

    スタッフ一覧で管理者を除外して表示の修正

--- src/resources/views/admin/staff/list.blade.php
+++ src/resources/views/admin/staff/list.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'スタッフ一覧')
 
 @section('content')
-<h2 class="title">▌スタッフ一覧</h2>
+<h2 class="title">スタッフ一覧</h2>
 
 <table>
     <thead>
@@ -15,11 +15,13 @@
     </thead>
     <tbody>
         @foreach ($users as $user)
+        @if (!empty($user->name))
         <tr>
             <td>{{ $user->name }}</td>
             <td>{{ $user->email }}</td>
             <td><a href="{{ route('admin.attendance.staff', $user->id) }}">詳細</a></td>
         </tr>
+        @endif
         @endforeach
     </tbody>
 </table>
